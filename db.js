143aaf687164d996eed184d8782c880719c2e0f8
GaneshPaidi-web <paidiganesh16905@gmail.com>
2026-03-15 13:30:33 +0530

    Refactor Database class and update password handling

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ class Database {
     this.pool = mysql.createPool({
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "ganesh@123",
+      password: process.env.DB_PASSWORD || "",
       database: process.env.DB_NAME || "test",
       multipleStatements: true
     });
